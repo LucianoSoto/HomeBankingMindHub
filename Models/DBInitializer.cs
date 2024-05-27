b7596f91dcc6859b37d190211c93079014d2b3df
Luciano Soto <lusoto456@gmail.com>
2024-05-27 12:20:34 -0300

    Agregar archivos de proyecto.

---
+++ Models/DBInitializer.cs
@@ -0,0 +1,23 @@
+﻿using HomeBankingMindHub.Models;
+
+namespace HomeBankingMindHub.Models
+{
+    public class DBInitializer
+    {
+        public static void Initialize(HomeBankingContext context)
+        {
+            if (!context.Clients.Any())
+            {
+                var clients = new Client[]
+                {
+                    new Client { Email = "vcoronado@gmail.com", FirstName="Victor", LastName="Coronado", Password="123456"},
+                    new Client { Email = "lusoto@gmail.com", FirstName="Luciano", LastName="Soto", Password="aaaaaaaa"}
+                };
+
+                context.Clients.AddRange(clients);
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
